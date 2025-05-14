91daedda3643bb3fd40357d6e35594e0190cd935
Ternovoi <danyloternovoi@gmail.com>
2025-05-14 11:01:45 +0300

    Initial Commit

---
+++ TESTTASK/TESTTASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTTASKApp.swift
+//  TESTTASK
+//
+//  Created by Danylo Ternovoi on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TESTTASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
